1ae626f871c301c73c46333d4a9d7d263ff2d92d
Sanjay  Tiwari <127861323+1589193@users.noreply.github.com>
2023-03-17 23:25:41 +0530

    Add files via upload

---
+++ Pattern18.java
@@ -0,0 +1,16 @@
+class Pattern18
+{
+public static void main(String args[])
+{ char ch;
+for(int i=5;i>=1;i--)
+{
+ ch='A';
+for(int j=1;j<=i;j++)
+ {
+ System.out.print(ch+" ");
+ ch++;
+ }
+ System.out.println();
+ }
+ }
+ }
